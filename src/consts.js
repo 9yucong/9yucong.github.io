f3062ba024ccfe743a517c177967298613ba18de
cong.gao <9yucong@gmail.com>
2023-03-23 16:22:03 +0800

    修改个人信息，删除例子文章

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
-export const SITE_TITLE = `Air Blog`;
-export const SITE_DESCRIPTION = 'Site Description';
-export const SITE_EMAIL = 'no.sql@qq.com'
-export const SITE_NAME = 'astro.yufengbiji.com';
-export const SITE_URL = "https://astro.yufengbiji.com";
+export const SITE_TITLE = `Cong Blog`;
+export const SITE_DESCRIPTION = 'Cong的博客';
+export const SITE_EMAIL = '1156557397@qq.com'
+export const SITE_NAME = '9yucong.github.io';
+export const SITE_URL = "https://9yucong.github.io";
